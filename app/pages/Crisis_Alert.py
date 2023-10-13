1badb5f951e946e2a2d207175bf9178a2bd3f47a
Anshika Jain <anshikajain2405@gmail.com>
2023-10-13 13:29:30 +0530

    added buttons, added nrows

--- app/pages/Crisis_Alert.py
+++ app/pages/Crisis_Alert.py
@@ -13,13 +13,27 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
+from streamlit_extras.switch_page_button import switch_page
 
 # Create a connection object.
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+df = conn.read(nrows=8)
 
 # Print results.
-for row in df.itertuples(index=True):
+for row in df.itertuples():
     if pd.notnull(row):
         st.write(f"{row.Facility}:  {row.Number}")
+
+# df = conn.read(
+#     worksheet="Sheet1",
+#     ttl="10m",
+#     usecols=[0, 1],
+#     ,
+# )
+
+col1, col2, col3 = st.columns([1,1,1])
+
+with col2:
+    if st.button('Help'):
+        switch_page('Help')
